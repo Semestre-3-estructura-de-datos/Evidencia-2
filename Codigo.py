3862e2f802c863bd4cb045a68de6c0ef9d79fad1
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-11-10 21:24:39 -0600

    Agrego el menu

--- Codigo.py
+++ Codigo.py
@@ -3,3 +3,15 @@ import sys
 import pandas as pd
 import csv
 import os
+
+menu=1
+separador=("*"*30)
+contador=1
+contadorventa=[]
+diccionariov={}
+try:
+    while menu==1:
+        print(separador+"Bienvenido al Menu Principal" +separador)
+        print("Opciones del menu")
+        print("1=Registrar una Venta\n2=Consultar ventas de un día específico\n3=Salir")
+        opcion=int(input("Que opcion eliges : "))
